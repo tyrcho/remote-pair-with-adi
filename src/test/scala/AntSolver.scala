06de37aabef731abd0bf8fcf22fbf1b0946fd216
Adi <adi.bolb@gmail.com>
2017-11-10 19:51:23 +0200

    Added many tests and not yet finished

--- src/test/scala/AntSolver.scala
+++ src/test/scala/AntSolver.scala
@@ -1,7 +1,26 @@
+
 object AntSolver {
+  def move(ants: List[Ant], time: Float) : List[Ant] = {
+    ants.map(_.move(time))
+  }
+
+
+  def computeCollisionTime(ant1: Ant, ant2: Ant): Option [Float] = {
+    val collisionFromLeftToRight = ant1.position > ant2.position && ant1.direction == LEFT && ant2.direction == RIGHT
+    val collisionFromRightToLeft = ant1.position < ant2.position && ant1.direction == RIGHT && ant2.direction == LEFT
+    if(collisionFromLeftToRight || collisionFromRightToLeft)
+      Some(math.abs(ant1.position - ant2.position) / (ant1.speed + ant2.speed))
+    else None
+  }
+
+  def solve(stickLength: Float, ant: Ant) = {
+    val distanceToGo = computeDistanceToGo(stickLength, ant.position, ant.direction)
+    computeTime(distanceToGo, ant.speed)
+  }
+
   def solve(stickLength: Float = 0, antSpeed: Float = 1, antPosition: Float = 0, direction: Direction = RIGHT): Float = {
-    val distanceToGo = computeDistanceToGo(stickLength, antPosition, direction)
-    computeTime(distanceToGo, antSpeed)
+    val ant = Ant(antPosition, direction, antSpeed)
+    solve(stickLength, ant)
   }
 
   def solve(ants: List[Ant], stickLength: Float): List[Float] =
@@ -27,4 +46,12 @@ object RIGHT extends Direction
 
 object LEFT extends Direction
 
-case class Ant(position: Float, direction: Direction, speed: Float)
+case class Ant(position: Float, direction: Direction, speed: Float){
+  def move (time: Float = 0) = {
+
+    val signum = if(direction == RIGHT) 1 else -1
+
+    val newPosition = position + signum * (speed * time);
+    copy(position = newPosition)
+  }
+}
--- src/test/scala/AntsSolverTest.scala
+++ src/test/scala/AntsSolverTest.scala
@@ -82,6 +82,13 @@ class AntsSolverTest extends FlatSpec with Matchers {
     time shouldBe 2F +- 0.00001F
   }
 
+  it should "check again that time is 2 for an ant with velocity 2, facing right and position 2 on a stick with length 6" in {
+    val ant = Ant(speed = 2, position = 2, direction = RIGHT);
+    val stickLength = 6
+    val time = AntSolver.solve(stickLength, ant)
+    time shouldBe 2F +- 0.00001F
+  }
+
   it should "check that times are (3,2) for 2 ants with velocity 1 and 1, facing RIGHT and positions 0 and 1 on a stick with length 3" in {
     val ant1 = Ant(speed = 1, direction = RIGHT, position = 0)
     val ant2 = Ant(speed = 1, direction = RIGHT, position = 1)
@@ -116,6 +123,107 @@ class AntsSolverTest extends FlatSpec with Matchers {
 
     times(0) shouldBe 2F +- 0.00001F
     times(1) shouldBe 2F +- 0.00001F
+  }
+
+  it should "detect there is not collision between two ants" in {
+    val ant1 = Ant(speed = 1, direction = LEFT, position = 0)
+    val ant2 = Ant(speed = 1, direction = RIGHT, position = 2)
+
+    val collisionTime = AntSolver.computeCollisionTime(ant1, ant2)
+
+    collisionTime shouldBe None
+
+  }
+
+  it should "detect there is one collision between two ants" in {
+    val ant1 = Ant(speed = 1, direction = RIGHT, position = 0)
+    val ant2 = Ant(speed = 1, direction = LEFT, position = 2)
+
+    val collisionTime = AntSolver.computeCollisionTime(ant1, ant2)
+
+    collisionTime.get shouldBe 1F +-0.00001F
+  }
+
+  it should "detect there is one collision between two ants reversed" in {
+    val ant1 = Ant(speed = 1, direction = RIGHT, position = 0)
+    val ant2 = Ant(speed = 1, direction = LEFT, position = 2)
+
+    val collisionTime = AntSolver.computeCollisionTime(ant2, ant1)
+
+    collisionTime.get shouldBe 1F +-0.00001F
+  }
+
+  it should "detect there is one collision between two ants further away" in {
+    val ant1 = Ant(speed = 1, direction = RIGHT, position = 0)
+    val ant2 = Ant(speed = 1, direction = LEFT, position = 4)
+
+    val collisionTime = AntSolver.computeCollisionTime(ant2, ant1)
+
+    collisionTime.get shouldBe 2F +-0.00001F
+  }
+
+  it should "detect there is one collision between two ants further away and different speeds" in {
+    val ant1 = Ant(speed = 1, direction = RIGHT, position = 0)
+    val ant2 = Ant(speed = 2, direction = LEFT, position = 3)
+
+    val collisionTime = AntSolver.computeCollisionTime(ant2, ant1)
+
+    collisionTime.get shouldBe 1F +-0.00001F
+  }
+
+  it should "detect there is one collision between two ants further away and different speeds reversed" in {
+    val ant1 = Ant(speed = 1, direction = RIGHT, position = 0)
+    val ant2 = Ant(speed = 2, direction = LEFT, position = 3)
+
+    val collisionTime = AntSolver.computeCollisionTime(ant1, ant2)
+
+    collisionTime.get shouldBe 1F +-0.00001F
+  }
+
+  it should("ant should move with zero speed for one second") in {
+    val ant = Ant(speed = 0, direction = RIGHT, position = 0)
+
+    val nextAnt = ant.move(time = 1)
+
+    nextAnt.position shouldBe 0F +- 0.00001F
+  }
+
+  it should("ant should move with speed 1 for one second") in {
+    val ant = Ant(speed = 1, direction = RIGHT, position = 0)
+
+    val nextAnt = ant.move(time = 1)
+
+    nextAnt.position shouldBe 1F +- 0.00001F
+  }
+
+  it should("ant should move with speed 2 for three seconds") in {
+    val ant = Ant(speed = 2, direction = RIGHT, position = 1)
+
+    val nextAnt = ant.move(time = 3)
+
+    nextAnt.position shouldBe 7F +- 0.00001F
+  }
+
+  it should("ant going left should move with speed 2 for three seconds") in {
+    val ant = Ant(speed = 2, direction = LEFT, position = 8)
+
+    val nextAnt = ant.move(time = 3)
+
+    nextAnt.position shouldBe 2F +- 0.00001F
+  }
+
+  it should("move two ants for 3 seconds") in {
+    val expectedAnt1 = Ant(speed = 2, direction = RIGHT, position = 7)
+    val expectedAnt2 = Ant(speed = 2, direction = LEFT, position = 2)
+    val newExpectedAnts = List(expectedAnt1, expectedAnt2)
+
+    val ant1 = Ant(speed = 2, direction = RIGHT, position = 1)
+    val ant2 = Ant(speed = 2, direction = LEFT, position = 8)
+    val ants = List(ant1, ant2)
+
+    val nextAnts = AntSolver.move(ants, time = 3)
+
+    nextAnts shouldBe newExpectedAnts
   }
 
 //  it should "check that ants collide when they have different speed" in {
